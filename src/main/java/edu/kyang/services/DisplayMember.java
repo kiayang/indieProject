c1bea719bfa59d702ac744d22fb213cbe2d75164
Kia Yang <kyang38@madisoncollege.edu>
2018-11-07 17:28:41 -0600

    Updated to remove comments for display member

--- src/main/java/edu/kyang/services/DisplayMember.java
+++ src/main/java/edu/kyang/services/DisplayMember.java
@@ -16,36 +16,19 @@ import java.io.IOException;
 import java.util.List;
 import java.util.logging.Logger;
 
-//@Path annotation is used to specify the URI through which a resource and an API can be accessed.
-//@Path("/hello")
-//reference to convert from object to json https://www.mkyong.com/java/jackson-2-convert-java-object-to-from-json/
 @Path("/users")
 public class DisplayMember {
 
-
-    //localhost:8080/indieProject/services/users/all <-full path to go to and should return all users
     @GET
-    //@Produces({"application/json", "text/plain", "application/xml"})
-
-    /**
-    @Path("/all/{param}")
-    public Response getMsg(@PathParam("param") String msg) {
-        String output = "'Jersey say ': '" + msg + "'}";
-        return Response.status(200).entity(output).build();
-
-    }
-    */
-
     @Path("/all")
     public Response getAllUsers() throws JsonProcessingException {
         GenericDAO userDAO = new GenericDAO(UserBean.class);
 
-        List<UserBean> usersList = userDAO.getAll(); //this is a List object with all users
+        List<UserBean> usersList = userDAO.getAll();
         ObjectMapper mapper = new ObjectMapper();
-        String responseString = mapper.writeValueAsString(usersList); //this converts your list object to json
-        return Response.status(200).entity(responseString).build(); //returns the json string as response -should show on full path i wrote above on line 20
+        String responseString = mapper.writeValueAsString(usersList);
+        return Response.status(200).entity(responseString).build();
 
     }
-
 
 }
